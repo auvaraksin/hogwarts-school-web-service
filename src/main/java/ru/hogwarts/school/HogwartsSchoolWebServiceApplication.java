132ffa96fe27404e092c5edc17db6dfc096b6d13
auvararaksin <auvaraksin@mail.ru>
2022-06-05 00:06:36 +0200

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/HogwartsSchoolWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchoolWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsSchoolWebServiceApplication.class, args);
+    }
+
+}
