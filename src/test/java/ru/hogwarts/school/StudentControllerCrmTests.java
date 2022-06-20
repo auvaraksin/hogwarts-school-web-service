de1d6f3d448a929871609d274c35b4728e21a067
auvararaksin <auvaraksin@mail.ru>
2022-06-20 20:49:33 +0200

    Version 1.2 HW_lesson_3.6 StudentControllerCrmTests's been updated TestConstants's been updated

--- src/test/java/ru/hogwarts/school/StudentControllerCrmTests.java
+++ src/test/java/ru/hogwarts/school/StudentControllerCrmTests.java
@@ -23,13 +23,10 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import static ru.hogwarts.school.TestConstants.*;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StudentControllerCrmTests {
-    String defaultURL;
-    String defaultRequestParameterStudentId = "?studentId=100";
-    String defaultRequestParameterByAge = "/filter/by-age?studentAge=33";
-    String defaultRequestParameterBetweenAges = "/filter/by-age?minAge=18&maxAge=40";
-    String defaultRequestParameterFacultyId = "/filter/by-faculty?facultyId=1";
 
     @LocalServerPort
     private int port;
@@ -48,7 +45,7 @@ class StudentControllerCrmTests {
 
     @BeforeEach
     void setup() {
-        defaultURL = "http://localhost:" + port + "/student";
+        DEFAULTURL = "http://localhost:" + port + "/student";
         Student student = new Student(100L, "Васийлий Уриевский", 33);
         Student student2 = new Student(200L, "Василий Супермен", 27);
         List<Student> students = Arrays.asList(student, student2);
@@ -74,69 +71,69 @@ class StudentControllerCrmTests {
 
     @Test
     public void getStudentById() throws Exception {
-        Assertions.assertNotNull(this.restTemplate.getForObject(defaultURL + defaultRequestParameterStudentId, String.class));
+        Assertions.assertNotNull(this.restTemplate.getForObject(DEFAULTURL + DEFAULTREQUESTPARAMETERSTUDENTID, String.class));
         String expectedResponse = "{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null}";
         ResponseEntity<String> responseOk
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterStudentId, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERSTUDENTID, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
         Assertions.assertEquals(expectedResponse, responseOk.getBody().toString());
         ResponseEntity<String> responseBadRequest
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterStudentId + "wrong", String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERSTUDENTID + "wrong", String.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseBadRequest.getStatusCode());
         ResponseEntity<String> responseInternalServerError
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterStudentId + 256, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERSTUDENTID + 256, String.class);
         Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseInternalServerError.getStatusCode());
     }
 
     @Test
     public void testGetStudentsList() throws Exception {
-        Assertions.assertNotNull(this.restTemplate.getForObject(defaultURL, String.class));
+        Assertions.assertNotNull(this.restTemplate.getForObject(DEFAULTURL, String.class));
         String expectedResponse = "[{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null},{\"id\":200,\"name\":\"Василий Супермен\",\"age\":27,\"faculty\":null}]";
         ResponseEntity<String> responseOk
-                = this.restTemplate.getForEntity(defaultURL, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
         Assertions.assertEquals(expectedResponse, responseOk.getBody().toString());
         ResponseEntity<String> responseNotFound
-                = this.restTemplate.getForEntity(defaultURL + "wrong", String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + "wrong", String.class);
         Assertions.assertEquals(HttpStatus.NOT_FOUND, responseNotFound.getStatusCode());
     }
 
     @Test
     public void testGetStudentListByAge() throws Exception {
-        Assertions.assertNotNull(this.restTemplate.getForObject(defaultURL + defaultRequestParameterByAge, String.class));
+        Assertions.assertNotNull(this.restTemplate.getForObject(DEFAULTURL + DEFAULTREQUESTPARAMETERBYAGE, String.class));
         String expectedResponse = "[{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null},{\"id\":200,\"name\":\"Василий Супермен\",\"age\":27,\"faculty\":null}]";
         ResponseEntity<String> responseOk
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterByAge, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERBYAGE, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
         Assertions.assertEquals(expectedResponse, responseOk.getBody().toString());
         ResponseEntity<String> responseBadRequest
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterByAge + "wrong", String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERBYAGE + "wrong", String.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseBadRequest.getStatusCode());
     }
 
     @Test
     public void testGetStudentListByAgesBetween() throws Exception {
-        Assertions.assertNotNull(this.restTemplate.getForObject(defaultURL + defaultRequestParameterBetweenAges, String.class));
+        Assertions.assertNotNull(this.restTemplate.getForObject(DEFAULTURL + DEFAULTREQUESTPARAMETERBETWEENAGES, String.class));
         String expectedResponse = "[{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null},{\"id\":200,\"name\":\"Василий Супермен\",\"age\":27,\"faculty\":null}]";
         ResponseEntity<String> responseOk
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterBetweenAges, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERBETWEENAGES, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
         Assertions.assertEquals(expectedResponse, responseOk.getBody().toString());
         ResponseEntity<String> responseBadRequest
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterBetweenAges + "wrong", String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERBETWEENAGES + "wrong", String.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseBadRequest.getStatusCode());
     }
 
     @Test
     public void testGetStudentListByFacultyId() throws Exception {
-        Assertions.assertNotNull(this.restTemplate.getForObject(defaultURL + defaultRequestParameterFacultyId, String.class));
+        Assertions.assertNotNull(this.restTemplate.getForObject(DEFAULTURL + DEFAULTREQUESTPARAMETERFACULTYID, String.class));
         String expectedResponse = "[{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null},{\"id\":200,\"name\":\"Василий Супермен\",\"age\":27,\"faculty\":null}]";
         ResponseEntity<String> responseOk
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterFacultyId, String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERFACULTYID, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
         Assertions.assertEquals(expectedResponse, responseOk.getBody().toString());
         ResponseEntity<String> responseBadRequest
-                = this.restTemplate.getForEntity(defaultURL + defaultRequestParameterFacultyId + "wrong", String.class);
+                = this.restTemplate.getForEntity(DEFAULTURL + DEFAULTREQUESTPARAMETERFACULTYID + "wrong", String.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseBadRequest.getStatusCode());
     }
 
@@ -145,11 +142,11 @@ class StudentControllerCrmTests {
         Student student = new Student();
         String expectedResponse = "{\"id\":100,\"name\":\"Васийлий Уриевский\",\"age\":33,\"faculty\":null}";
         String response
-                = this.restTemplate.postForObject(defaultURL, student, String.class);
+                = this.restTemplate.postForObject(DEFAULTURL, student, String.class);
         Assertions.assertNotNull(response);
         Assertions.assertEquals(expectedResponse, response);
         HttpEntity<Student> entity = new HttpEntity<Student>(student);
-        ResponseEntity<Student> responseOK = this.restTemplate.exchange(defaultURL, HttpMethod.POST, entity, Student.class, 100L);
+        ResponseEntity<Student> responseOK = this.restTemplate.exchange(DEFAULTURL, HttpMethod.POST, entity, Student.class, 100L);
         Assertions.assertEquals(HttpStatus.OK, responseOK.getStatusCode());
         Assertions.assertEquals("Васийлий Уриевский", responseOK.getBody().getName());
     }
@@ -159,7 +156,7 @@ class StudentControllerCrmTests {
         Student student = new Student(100L, "Васийлий Уриевский", 33);
         when(studentRepository.existsById(any(Long.class))).thenReturn(true);
         HttpEntity<Student> entity = new HttpEntity<Student>(student);
-        ResponseEntity<Student> responseOK = this.restTemplate.exchange(defaultURL, HttpMethod.PUT, entity, Student.class, 100L);
+        ResponseEntity<Student> responseOK = this.restTemplate.exchange(DEFAULTURL, HttpMethod.PUT, entity, Student.class, 100L);
         Assertions.assertNotNull(responseOK.getBody().getId());
         Assertions.assertEquals(HttpStatus.OK, responseOK.getStatusCode());
         Assertions.assertEquals("Васийлий Уриевский", responseOK.getBody().getName());
@@ -169,7 +166,7 @@ class StudentControllerCrmTests {
     public void testDeleteStudent() {
         Student student = new Student();
         HttpEntity<Student> entity = new HttpEntity<Student>(student);
-        ResponseEntity<String> responseOk = this.restTemplate.exchange(defaultURL + "/100", HttpMethod.DELETE, entity, String.class);
+        ResponseEntity<String> responseOk = this.restTemplate.exchange(DEFAULTURL + "/100", HttpMethod.DELETE, entity, String.class);
         Assertions.assertEquals(HttpStatus.OK, responseOk.getStatusCode());
     }
 
--- src/test/java/ru/hogwarts/school/TestConstants.java
+++ src/test/java/ru/hogwarts/school/TestConstants.java
@@ -5,6 +5,12 @@ import org.json.JSONObject;
 import ru.hogwarts.school.model.Faculty;
 
 public class TestConstants {
+    public static String DEFAULTURL;
+    public final static String DEFAULTREQUESTPARAMETERSTUDENTID = "?studentId=100";
+    public final static String DEFAULTREQUESTPARAMETERBYAGE = "/filter/by-age?studentAge=33";
+    public final static String DEFAULTREQUESTPARAMETERBETWEENAGES = "/filter/by-age?minAge=18&maxAge=40";
+    public final static String DEFAULTREQUESTPARAMETERFACULTYID = "/filter/by-faculty?facultyId=100";
+
     public final static Long ID_DEFAULT = 1L;
     public final static String NAME_DEFAULT = "ANewOne";
     public final static String COLOR_DEFAULT = "Green";
