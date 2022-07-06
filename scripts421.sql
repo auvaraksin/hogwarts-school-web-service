6b56d99d31d4b0515a777dec5aebbd2df47bc77c
auvararaksin <auvaraksin@mail.ru>
2022-07-06 10:25:47 +0200

    Initial version HW_lesson_4.2 Scripts421.sql's been modifided according to the mentor's comment

--- scripts421.sql
+++ scripts421.sql
@@ -2,6 +2,8 @@ ALTER TABLE student ADD CONSTRAINT age_constraint CHECK (age >= 16);
 
 ALTER TABLE student ADD CONSTRAINT name_constraint UNIQUE (name);
 
+ALTER TABLE student ALTER COLUMN name SET NOT NULL;
+
 ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
 
 ALTER TABLE faculty ADD CONSTRAINT name_color_faculty_constraint UNIQUE (name, color);
